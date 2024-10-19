40f4c33107840bcb626fd19b454f788869e0644f
Матфей Лисов Игоревич <lmatfey11@gmail.com>
2024-10-19 07:04:58 +0300

    button_list

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from tkinter import *
 
+#длина и ширина и расположение
+#root.geometry('400x300+500+200')
+#разрешение на изменение окна
+#root.resizable(True, False)
+#максимальные и минимальные значения размеров окна
+#root.minsize(300, 150)
+#root.maxsize(600, 450)
+
+def puch(a):
+    if 
 
 game = [None] * 9
 game_left = list(range(9))
@@ -7,13 +17,19 @@ turn = 0
 
 root = Tk()
 label = Label(width=20, text = 'Крестики-нолики')
+button = [Button(width=4, height=2, font=('Arial', 25, 'bold'), bg="pink", command=lambda x=i: puch(x)) for i in range(9)]
+label.grid(row=0, column=0, columnspan=3)
 
-#длина и ширина и расположение
-root.geometry('400x300+500+200')
-#разрешение на изменение окна
-root.resizable(True, False)
-#максимальные и минимальные значения размеров окна
-root.minsize(300, 150)
-root.maxsize(600, 450)
+row = 1
+col = 0
+
+for i in range(9):
+    button[i].grid(row=row, column=col)
+    col += 1    
+    if col == 3:
+        row += 1
+        col = 0
+
+
 
 root.mainloop()
